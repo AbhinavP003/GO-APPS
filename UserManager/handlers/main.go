a8d771760dca9abb919fad354490067fa15f66d3
Abhinav P <abhinavp003@gmail.com>
2024-10-16 15:15:52 +0530

    Created first sample api

---
+++ UserManager/handlers/main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	r := gin.Default()
+	r.GET("/", func(ctx *gin.Context) {
+		ctx.JSON(http.StatusOK, gin.H{
+			"message": "ok"})
+	})
+	port := "3000"
+	r.Run(":" + port)
+}
